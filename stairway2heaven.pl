1ab11ddf75db33be16d1848c0d438ca0ae9b2912
Tsili42 <30462705+Tsili42@users.noreply.github.com>
2017-08-20 19:57:27 +0300

    Update stairway2heaven.pl

--- stairway2heaven.pl
+++ stairway2heaven.pl
@@ -26,24 +26,45 @@ read_line(Stream, List) :-
     ; atom_codes(A, Line),
       atomic_list_concat(As, ' ', A),
       maplist(atom_number, As, List)
-    ).
-findWays(_, 0, _, [], _, 0).
-findWays(N, _, _, _, _, W):-
-    (N =< 1, W is N
-    ;1 is 0
-    ).
-findWays(N, K, Br, [A|B], Broken, Ways):-
-    New_N is N - A,
-    (\+ member(New_N, Broken),
-     findWays(New_N, K, Br, [A|B], Broken, Ways1),
-     New_K is K - 1,
-     findWays(N, New_K, Br, B, Broken, Ways2),
-     Ways is Ways1 + Ways2
-     ; New_K is K - 1,
-     findWays(N, New_K, Br, B, Broken, Ways2),
-     Ways is Ways2
+   ).
+
+countWays(N, N, _, _, _, Res, Ways):-
+    Correct_N is N - 1,
+    get_assoc(Correct_N, Res, Ways).
+countWays(I, N, [], HelpSteps, ABroken, Res, Ways):-
+    New_I is I + 1,
+    countWays(New_I, N, HelpSteps, HelpSteps, ABroken, Res, Ways),!.
+countWays(I, N, [StA|StB], HelpSteps, ABroken, Res, Ways):-
+    /*print('S'),
+    print(I),*/
+    ( StA =< I,
+      Dif is I - StA,
+      ( \+ get_assoc(Dif, ABroken, Dif),
+        get_assoc(Dif, Res, Add1),
+        get_assoc(I, Res, Base),
+        New_Res0 is Base + Add1,
+        New_Res is New_Res0 mod 1000000009,
+       /* print(New_Res),
+        print(,),*/
+        put_assoc(I, Res, New_Res, Resminus),
+        countWays(I, N, StB, HelpSteps, ABroken, Resminus, Ways),!
+      ; countWays(I, N, StB, HelpSteps, ABroken, Res, Ways),!
+      )
+    ; StA > I,
+      countWays(I, N, StB, HelpSteps, ABroken, Res, Ways),!
     ).
 
 hopping(File, Ways):-
-    read_input(File, N, K, B, Steps, Broken),
-    findWays(N, K, B, Steps, Broken, Ways).
+    read_input(File, N, _, _, Steps, Broken),
+    pairs_keys_values(Pairs, Broken, Broken),
+    list_to_assoc(Pairs, ABroken),
+    New_N is N + 1,
+    numlist(0, New_N, X),
+    Nplus2 is New_N + 1,
+    length(Zeros, Nplus2),
+    maplist(=(0), Zeros),
+    pairs_keys_values(DesPairs, X, Zeros),
+    list_to_assoc(DesPairs, Res0),
+    put_assoc(1, Res0, 1, Res),
+    countWays(2, New_N, Steps, Steps, ABroken, Res, Ways).
+
